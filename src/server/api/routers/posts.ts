06eec22a8e7bfc7b9a428641bfff703546c43290
devsnowin <snowin.developer@gmail.com>
2023-03-24 22:21:10 +0530

    added: CI

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,6 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { string, z } from "zod";
+import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
